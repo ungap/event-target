e39a19bbef616f70126f5e3cf12871790592b89a
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2020-06-21 12:33:59 +0200

    removed self dance in modules

--- esm/index.js
+++ esm/index.js
@@ -1,5 +1,5 @@
 /*! (c) Andrea Giammarchi - ISC */
-var self = this || /* istanbul ignore next */ {};
+var self = {};
 try {
   self.EventTarget = (new EventTarget).constructor;
 } catch(EventTarget) {
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*! (c) Andrea Giammarchi - ISC */
-var self = this || /* istanbul ignore next */ {};
+var self = this || {};
 try {
   self.EventTarget = (new EventTarget).constructor;
 } catch(EventTarget) {
